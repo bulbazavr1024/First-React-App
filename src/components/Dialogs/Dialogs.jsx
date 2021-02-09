6f89a5f57e14e565a0900ff2ed73a6cfc81d920b
bulbazavr1337 <pavel.ageykin@yandex.ru>
2021-02-09 16:18:23 +0300

    After adding messages and NavLink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,33 @@ import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div className={s.content}>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        Sergey
+                    </div>
+                    <div className={s.dialog}>
+                        Pavel
+                    </div>
+                    <div className={s.dialog}>
+                        Anna
+                    </div>
+                    <div className={s.dialog}>
+                        Ivan
+                    </div>
+                    <div className={s.dialog}>
+                        Sveta
+                    </div>
+
+
+
+                </div>
+                <div className={s.messages}>
+                    <div className="message">Hi</div>
+                    <div className="message">How are you?</div>
+                    <div className="message">Yo</div>
+                </div>
+            </div>
         </div>
 
     )
